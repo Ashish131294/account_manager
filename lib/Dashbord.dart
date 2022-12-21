c28cdd02334630222d03de0ee4435c86b49a131d
Ashish131294 <ashish.kachhadiya1114@gmail.com>
2022-12-21 09:20:18 +0530

    Initial commit

--- lib/Dashbord.dart
+++ lib/Dashbord.dart
@@ -12,8 +12,10 @@ class dashbord extends StatefulWidget {
 }
 
 class _dashbordState extends State<dashbord> {
-  List<Map<String, Object?>> l = List.empty();
+  List<Map<String, Object?>> l = [];
   Database? db;
+  bool status = false;
+  TextEditingController tname = TextEditingController();
 
   @override
   void initState() {
@@ -23,7 +25,7 @@ class _dashbordState extends State<dashbord> {
 
   getalldata() async {
     // Get the records
-    Database db = await Dbhelper().createDatabase();
+    db = await Dbhelper().createDatabase();
     String qry = "SELECT * FROM Test";
     List<Map<String, Object?>> l1 = await db!.rawQuery(qry);
     l.addAll(l1);
@@ -32,112 +34,138 @@ class _dashbordState extends State<dashbord> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dashbord"),
-          /*actions: [
-          IconButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                               //useSafeArea: true,
-                  //barrierColor: Colors.transparent,
-                  builder: (context) {
-                    return Column(
+        appBar: AppBar(title: Text("Dashbord"),
+            actions: [
+              PopupMenuButton(
+                itemBuilder: (context) => [
+                  PopupMenuItem(child: Text("Save as PDF")),
+                  PopupMenuItem(child: Text("Save as Excel")),
+                ],
+              )
+            ]),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  mydrawer(),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.backup),
+              title: Text("Backup"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.restore),
+              title: Text("Restore"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.change_circle),
+              title: Text("Change Currency"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.phone_android_rounded),
+              title: Text("Change Security Question"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Setting"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text("Share the App"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.star_rate),
+              title: Text("Rate the App"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.more_vert),
+              title: Text("More App"),
+              onTap: () {},
+            ),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return SimpleDialog(title: Text("Add New Account",),
+                  children: [
+                    /*AppBar(
+                      title: Text("Add New Account"),
+                    ),*/
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: tname,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Account Name"),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        TextButton(
-                            onPressed: () {}, child: Text("Save as PDF")),
-                        TextButton(
-                            onPressed: () {}, child: Text("Save as Excel")),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(context, MaterialPageRoute(
+                                  builder: (context) {
+                                    return dashbord();
+                                  },
+                                ));
+                              },
+                              child: Text("Cancel")),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                String name = tname.text;
+
+                                String qry ="insert into Test(name) values('$name')";
+                                int a=await db!.rawInsert(qry);
+                                print(a);
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                                  return dashbord();
+                                },));
+                              },
+                              child: Text("Save")),
+                        )
                       ],
-                    );
-                  },
+                    )
+                  ],
                 );
               },
-              icon: Icon(Icons.more_vert))
-        ],*/
-          actions: [
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(child: Text("Save as PDF")),
-                PopupMenuItem(child: Text("Save as Excel")),
-              ],
-            )
-          ]),
-      drawer: Drawer(
-          child: ListView(
-        children: [
-          Container(
-            child: Column(
-              children: [
-                mydrawer(),
-              ],
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.backup),
-            title: Text("Backup"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.restore),
-            title: Text("Restore"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.change_circle),
-            title: Text("Change Currency"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.phone_android_rounded),
-            title: Text("Change Security Question"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Setting"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.share),
-            title: Text("Share the App"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.star_rate),
-            title: Text("Rate the App"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.more_vert),
-            title: Text("More App"),
-            onTap: () {},
-          ),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(
-            builder: (context) {
-              return insert();
-            },
-          ));
-        },
-        child: Icon(Icons.add),
-      ),
-      body: GridView.builder(
+            ));
+          },
+          child: Icon(Icons.add),
+        ),
+        body: ListView.builder(
+          itemCount: l.length,
           itemBuilder: (context, index) {
             Map m = l[index];
             return ListTile(
-              leading: Text("${m['id']}"),
+              leading: Text("${m['name']}"),
+
             );
           },
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3)),
-    );
+        ));
   }
 }
